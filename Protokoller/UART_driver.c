d9672070c8fae333d1eced27123677c545fbdf57
Andreas Kjøs Seljeset <andrksel@ntnu.no>
2026-09-08 17:10:07 +0200

    Added Interrupts and reorganized

--- Protokoller/UART_driver.c
+++ Protokoller/UART_driver.c
@@ -5,9 +5,14 @@
  * Created: 08.09.2026 10:08:04
  *  Author: andrksel
  */ 
-#include <avr/io.h>
 #include "UART_driver.h"
 
+//
+ISR(USART0_RXC_vect)
+{
+	unsigned char data = UDR0; //Received byte
+	if (data == 'a'){ uart_printf(data);}
+}
 
 
 void uart_init(void)
@@ -16,23 +21,36 @@ void uart_init(void)
 	UBRR0H = (unsigned char)(UBRR_VALUE >> 8);
 	UBRR0L = (unsigned char)UBRR_VALUE;
 	
+	
+	//Page 186/187
 	// Enable receiver and transmitter
 	UCSR0B = (1 << RXEN0) | (1 << TXEN0);
+	
+	//Enable Local Read interrupts
+	UCSR0B = UCSR0B | (1 << RXCIE0);
+	UCSR0A = UCSR0A | (1 << RXC0);
+		
 
 	// Frame format: 8 data bits, 1 stop bit, no parity
-	//UCSR0C = (1 << UCSZ01) | (1 << UCSZ00);
-	// Frame format: 8 data bits, 1 stop bit, no parity
-	UCSR0C = (1<<URSEL0)|(1<<USBS0)|(3<<UCSZ00);
+	UCSR0C = (1<<URSEL0) | (1 << UCSZ01) | (1 << UCSZ00); //- tidligere feil
+	//UCSR0C = (1<<URSEL0)|(1<<USBS0)|(3<<UCSZ00); //sjekk stop bit
+			
 }
 
 unsigned char uart_receive(void)
 {
 	// Wait for data to be received
-	while (!(UCSR0A & (1 << RXC0)));
+	while ((UCSR0A & (1 << RXC0)));
 
 	// Get and return received data from buffer
 	return UDR0;
 }
+
+
+void uart_printf(char c){
+	UDR0 = c;
+}
+
 
 void uart_transmit(unsigned char data)
 {
@@ -41,6 +59,8 @@ void uart_transmit(unsigned char data)
 
 	// Put data into buffer, sends the data
 	UDR0 = data;
+	
+	
 }
 
 
@@ -51,5 +71,3 @@ void uart_print(const char *str)
 		uart_transmit(*str++); //Bruker tegnet str peker på deretter oeker pekeren til neste tegn.
 	}
 }
-
-//fdevopen(transmit function, receive function);
